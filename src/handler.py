d89a623c49e6a107b510fffbe713af9b47d8a190
Bogdan Drema <zurnaz@gmail.com>
2019-08-07 18:33:14 +0100

    Linting fix

--- src/handler.py
+++ src/handler.py
@@ -1,11 +1,19 @@
+"""
+Demo handler for serverless api
+"""
 import json
 # https://stackoverflow.com/a/50034121
 # import boto3
 # from datetime import datetime
 
+#pylint: disable=unused-argument
+
 
 def post_request(event, context):
-    # TODO add error handling for invalid inputs
+    """
+    POST data here
+    TODO add error handling for invalid inputs
+    """
     body = json.loads(event['body'])
     result = body
     return {
@@ -16,6 +24,9 @@ def post_request(event, context):
 
 
 def get_request(event, context):
+    """
+    GET data from here
+    """
     return {
         'statusCode': 200,
         'headers': {'Access-Control-Allow-Origin': '*'},
