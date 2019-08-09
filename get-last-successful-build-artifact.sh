52091a1940af2da8c79f90b30fac046de1eb0328
Bogdan Drema <zurnaz@gmail.com>
2019-08-09 16:19:46 +0100

    Terrible debugging attempt

--- get-last-successful-build-artifact.sh
+++ get-last-successful-build-artifact.sh
@@ -4,7 +4,7 @@ set -e
 
 # check requirements
 REQS=( curl jq )
-echo $REQS
+echo "1"
 for REQ in ${REQS[@]}
 do
   which ${REQ} >/dev/null
@@ -13,7 +13,7 @@ do
     exit 1
   fi
 done
-
+echo "2"
 # set vars in project settings
 BASE_URL=${BASE_URL:-$(echo $CI_PROJECT_URL |  cut -d'/' -f1-3)}
 VARS=( BASE_URL PRIVATE_TOKEN PROJECT STAGE )
@@ -21,22 +21,24 @@ VARS=( BASE_URL PRIVATE_TOKEN PROJECT STAGE )
 . <(curl -fsSL https://gitlab.com/morph027/gitlab-ci-helpers/raw/master/check-vars.sh)
 
 check_vars
-
+echo "3"
 # fetch last successful build
 
 DEFAULT_FILTER=".[] | select(.status==\"success\")  | select(.stage==\"${STAGE}\") | select(.ref==\"${REF:-develop}\")"
 FILTER="$DEFAULT_FILTER"
-
+echo "4"
 if [ ! -z $COMMIT ]; then
   FILTER+=" | select(.commit.short_id==\"$COMMIT\")"
 fi
-
+echo "5"
 LAST_SUCCESSFUL_BUILD=$(curl -s -H "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" "${BASE_URL}/api/v4/projects/${PROJECT}/jobs?per_page=${PER_PAGE:-50}" | jq -c ''"$FILTER"' | {id}' | head -n1 | grep -oE '[0-9]+')
 echo $LAST_SUCCESSFUL_BUILD
-
+echo "6"
 # download
 download_latest() {
   [ -z $OUT_FILE ] && OUT_FILE="artifacts.zip"
   curl -fksSL -o ${OUT_FILE} -H "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" "${BASE_URL}/api/v4/projects/${PROJECT}/jobs/${LAST_SUCCESSFUL_BUILD}/artifacts"
 }
+echo "7"
 download_latest
+echo "8"
